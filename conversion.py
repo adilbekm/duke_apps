8e63da02b67d186e1b12efa979752ed8c66ca43a
adilbekm <adilbekm@yahoo.com>
2017-03-28 22:18:47 -0400

    Update conversion.py to use additional input file
    input_subs_countries.txt contains country information per each
    subrecipient, e.g. 1372 US, 4728 AU, etc. that program loads
    into a dictionary and use to populate appropriate field in output.

--- conversion.py
+++ conversion.py
@@ -6,7 +6,7 @@
 # = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 # INPUTS:
-# input_subs.txt, input_invs.txt
+# input_subs.txt, input_invs.txt, input_zfr1d.txt, input_subs_countries.txt
 # OUTPUTS:
 # output_subs.txt, output_subs_details.txt, output_invs.txt, output_invs_details.txt
 # OPTIONAL FILES:
@@ -50,18 +50,24 @@ except OSError: pass
 try: os.remove('output_invs_details.txt')
 except OSError: pass
 
-if not os.path.exists('input_subs.txt') or not os.path.exists('input_invs.txt'):
-	print('Cannot find input files input_subs.txt and/or input_invs.txt')
-	print('-' * 50); print('Program terminated early'); print('-' * 50)
-	exit()
+for input_fname in ['input_subs.txt', 'input_invs.txt', 'input_zfr1d.txt', 'input_subs_countries.txt']:
+	if not os.path.exists(input_fname):
+		print('Some input files are missing:')
+		print('input_subs.txt, input_invs.txt, input_zfr1d.txt, or input_subs_countries.txt')
+		print('-' * 51); print('Program terminated early'); print('-' * 51)
+		exit()
 
 sleep(1)
 
 # open needed files
 input_subs = open('input_subs.txt', encoding='utf8')
 input_invs = open('input_invs.txt', encoding='utf8')
+input_zfr1d = open('input_zfr1d.txt', encoding='utf8')
+input_subs_countries = open('input_subs_countries.txt', encoding='utf8')
+
 log_file = open('log.txt', 'a', encoding='utf8')
 log_file.write('----- Start: ' + str(datetime.now()) + ' -----\n')
+
 outfile_subs = open('output_subs.txt', 'a', encoding='utf8')
 outfile_subs_details = open('output_subs_details.txt', 'a', encoding='utf8')
 outfile_invs = open('output_invs.txt', 'a', encoding='utf8')
@@ -127,6 +133,15 @@ def text_to_date(text):
 	text = text.strip()[0:10]
 	return datetime.strptime(text, '%m/%d/%Y')
 
+def is_date_reasonable(date_val):
+	'''
+	Accepts a date and checks if value is within a reasonable range.
+	'''
+	if (date_val < datetime(year=1980, month=1, day=1) or
+			date_val > datetime(year=2040, month=12, day=31)):
+		return 0
+	return 1
+
 def check_budget_periods(sub):
 	'''
 	Accepts a sub and returns a two-element tuple where the first
@@ -134,6 +149,9 @@ def check_budget_periods(sub):
 	and the second value is either None (if the first value = 0) or
 	the same or updated sub record (updated if found and fixed any
 	missing start dates).
+
+	A budget period is considered valid and included in the count
+	of budget periods only if both start and end dates are available.
 	'''
 	valid_periods = 0
 	# initial_sub = sub
@@ -203,6 +221,32 @@ raw_invs = input_invs.readlines()
 count = len(raw_invs)
 print(padded_text(count, len(to_print)))
 
+# read zfr1d list into a variable
+to_print = 'Reading zfr1d records...'
+print(to_print, end='')
+raw_zfr1d_recs = input_zfr1d.readlines()
+zfr1d_recs = []
+for rec in raw_zfr1d_recs:
+	rec = rec.strip()
+	zfr1d_recs.append(rec)
+count = len(zfr1d_recs)
+print(padded_text(count, len(to_print)))
+# cleanup
+del raw_zfr1d_recs
+
+to_print = 'Reading subaward country data...'
+print(to_print, end='')
+raw_lines = input_subs_countries.readlines()
+subs_countries = {}
+for line in raw_lines:
+	line = line.strip()
+	line = line.split()
+	subs_countries[int(line[0])] = line[1]
+count = len(subs_countries)
+print(padded_text(count, len(to_print)))
+# cleanup
+del raw_lines
+
 # check that sub records have correct number of fields (96)
 to_print = 'Checking subawards...'
 print(to_print, end='')
@@ -246,7 +290,6 @@ for sub in raw_subs:
 		remaining_sub_ids.append(sub[0])
 		cleaner_subs.append(raw_sub)
 print(padded_text(i, len(to_print)))
-
 # cleanup
 subs = cleaner_subs
 del cleaner_subs
@@ -266,7 +309,6 @@ for inv in raw_invs:
 	else:
 		i += 1
 print(padded_text(i, len(to_print)))
-
 # cleanup
 invs = clean_invs
 del clean_invs
@@ -287,10 +329,9 @@ for sub in subs:
 	else:
 		dropped_subs.append(sub)
 print(padded_text(i, len(to_print	)))
-to_print = 'Subs dropped for not having any periods...'
+to_print = 'Subs dropped for not having valid periods...'
 print(to_print, end='')
 print(padded_text(len(dropped_subs), len(to_print)))
-
 # cleanup
 subs = clean_subs
 del clean_subs
@@ -315,7 +356,6 @@ for sub in subs:
 		remaining_sub_ids.append(sub[0])
 		clean_subs.append(raw_sub)
 print(padded_text(i, len(to_print)))
-
 # cleanup
 subs = clean_subs
 del clean_subs
@@ -334,16 +374,48 @@ for inv in invs:
 	else:
 		i += 1
 print(padded_text(i, len(to_print)))
-
 # cleanup
 invs = clean_invs
 del clean_invs
 del remaining_sub_ids
 
+to_print = 'Updating subs with zfr1d data...'
+print(to_print, end='')
+upd_subs = []
+for sub in subs:
+	raw_sub = sub.strip()
+	sub = raw_sub.split('|')
+	if sub[1] in zfr1d_recs:
+		upd_subs.append(raw_sub + '|ADJ')
+	else:
+		upd_subs.append(raw_sub + '|DB')
+print(padded_text('OK', len(to_print)))
+# cleanup
+subs = upd_subs
+del upd_subs
+del zfr1d_recs
+
+to_print = 'Updating subs with correct country codes...'
+print(to_print, end='')
+upd_subs = []
+for sub in subs:
+	raw_sub = sub.strip()
+	sub = raw_sub.split('|')
+	subtor_id = int(sub[2])
+	country_code = subs_countries.get(subtor_id, 'US') # default is US
+	sub[21] = country_code
+	sub = '|'.join(sub)
+	upd_subs.append(sub)
+print(padded_text('OK', len(to_print)))
+# cleanup
+subs = upd_subs
+del upd_subs
+
 # check for include/exclude list
 # if include list exists, only include those subs (and their invs) in the output
 # if exclude list exists, excluding those subs
 if subs_include:
+
 	subs_include = subs_include.readlines()
 	clean_subs_include = []
 	for sub in subs_include:
@@ -380,6 +452,7 @@ if subs_include:
 	del clean_invs
 
 elif subs_exclude:
+
 	subs_exclude = subs_exclude.readlines()
 	clean_subs_exclude = []
 	for sub in subs_exclude:
@@ -433,10 +506,12 @@ print(padded_text(len(subs), len(to_print)))
 to_print = 'Number of invoices to convert:'; print(to_print, end='')
 print(padded_text(len(invs), len(to_print)))
 
+output1_count = 0
 output2_count = 0
 output3_count = 0
 output4_count = 0
-# output1_count is just the length of subs list
+subs_dropped_for_zero_budgets = 0
+invs_dropped_for_zero_amount = 0
 
 # write file headers
 header_subs = 'WBSE|State|Country|Subaward Number|FFATA|Final Invoice Due|G/L Break|Prior Year WBSE|OSP Notes|IDC Default|Received Date|Subrecipient PI Name|Manual Prior Exp|Type of Subaward|Type of Payment|Invoice Requirements|Equipment|Budgetary Changes|Budget Restrictions|Special T&C'
@@ -464,28 +539,44 @@ for sub in subs:
 	out_subs.append(str(gl_break))       # gl break
 	out_subs.append(sub[11].strip())     # prior year wbse
 	out_subs.append(sub[10].strip())     # osp notes
-	out_subs.append('') 						     # skip row
-	out_subs.append('') 						     # skip row
+	out_subs.append('X') 						     # idc default (always X)
+	current_date = datetime.now()
+	current_date = current_date.strftime('%m/%d/%Y')
+	out_subs.append(current_date) 	     # received date (use current date)
 	out_subs.append('') 						     # skip row
 	prior_exp = text_to_float(sub[13])
 	out_subs.append(str(prior_exp))      # manual prior exp
 
 	out_sub = '|'.join(out_subs)
-	outfile_subs.write(out_sub + '\n')
+	# actual write to output happens after looping through budget periods. 
 
+	# the most recent budget period should be 9, one prior to it - 8, etc.
 	num_periods = int(sub[96])
 	first_period = 10 - num_periods
 
+	# go through each valid budget period and collect line items for each. 
+	# if line items have values - write the period to the output file, 
+	# and conversely, if no values in any line items - skip that period.
+	non_zero_periods_exist = 0
 	for i in range(0, num_periods):
 
 		wbse = sub[1].strip()
 		fisc_per = str(first_period + i)
 		fisc_yr = str(2017)
 		start = sub[24 + i].strip()[0:10]
+		if not is_date_reasonable(text_to_date(start)):
+			log_file.write('Sub wbse=' + wbse + ' has an unreasonable Start date: ' + start + '\n')
 		end = sub[30 + i].strip()[0:10]
+		if not is_date_reasonable(text_to_date(end)):
+			log_file.write('Sub wbse=' + wbse + ' has an unreasonable End date: ' + end + '\n')
 		idc_rate = sub[72 + i]
 		idc_rate = text_to_float(idc_rate)
 		idc_rate_reformatted = str(round((idc_rate * 100), 2))
+
+		# check if end date is greater than start date; make a log entry if true
+		dates_diff = text_to_date(end) - text_to_date(start)
+		if dates_diff <= timedelta(days = 0):
+			log_file.write('Sub wbse=' + wbse + ' has a strange Start-End date range\n')
 
 		line_items = []
 
@@ -566,10 +657,21 @@ for sub in subs:
 
 		# write to file
 		for amount, category in line_items:
+			non_zero_periods_exist += 1
 			sub_detail = [wbse, fisc_per, fisc_yr, start, end, amount, category, idc_rate_reformatted]
 			sub_detail = '|'.join(sub_detail)
 			outfile_subs_details.write(sub_detail + '\n')
 			output2_count += 1
+
+	if non_zero_periods_exist:
+		# write the sub record to output
+		outfile_subs.write(out_sub + '\n')
+		output1_count += 1
+	else:
+		# the sub doesn't have any non-zero budgets; skip to next sub.
+		log_file.write('Sub wbse=' + wbse + ' skipped for not having any non-zero budgets\n')
+		subs_dropped_for_zero_budgets += 1
+		continue
 
 	# collect invoices for the given sub
 	sub_invs = []
@@ -612,7 +714,8 @@ for sub in subs:
 			total_inv_amount = direct_cost + idc + sum(line_items[8:])
 
 			if not total_inv_amount:
-				log_file.write('Inv ' + str(inv[0]) + ' skipped because total amt = 0' + '\n')
+				# log_file.write('Inv ' + str(inv[0]) + ' skipped because total amt = 0\n')
+				invs_dropped_for_zero_amount += 1
 				continue
 
 			line_items[6] = idc # replace idc_rate with idc amount
@@ -621,22 +724,37 @@ for sub in subs:
 			out_invs = []
 
 			out_invs.append(sub[1].strip())         # wbse
-			out_invs.append(inv[2].strip())         # invoice number
+			inv_num = inv[2].strip()
+			out_invs.append(inv_num)                # invoice number
 			out_invs.append(inv[3].strip())         # ap check req number
-			out_invs.append(inv[6].strip()[0:10])   # received date
+			rec_date = inv[6].strip()[0:10]
+			out_invs.append(rec_date)               # received date
+			if rec_date:
+				if not is_date_reasonable(text_to_date(rec_date)):
+					log_file.write('Inv id=' + str(round(inv[0])) + ' has an unreasonable Received date: ' + rec_date + '\n')
 			out_invs.append(inv[10].strip())        # final
 			out_invs.append('')                     # skip row (treat as final)
 			out_invs.append(inv[11].strip())        # initially accurate
 			out_invs.append('')                     # skip row (vendor)
 			out_invs.append('')                     # skip row (wire draft)
 			out_invs.append(inv[9].strip())         # notes
-			out_invs.append(inv[23].strip()[0:10])  # start date
+			start_date = inv[23].strip()[0:10]
+			out_invs.append(start_date)             # start date
+			if not is_date_reasonable(text_to_date(start_date)):
+				log_file.write('Inv id=' + str(round(inv[0])) + ' has an unreasonable Start date: ' + start_date + '\n')
 			end_date = inv[24].strftime('%m/%d/%Y')
 			out_invs.append(end_date)               # end date
-			out_invs.append('DB') 						      # skip row (osp invoice type)
+			if not is_date_reasonable(text_to_date(end_date)):
+				log_file.write('Inv id=' + str(round(inv[0])) + ' has an unreasonable End date: ' + end_date + '\n')
+			out_invs.append(sub[97].strip()) 	      # osp invoice type
 			idcr = text_to_float(inv[31])
 			idcr_reformatted = round((idcr * 100), 2)
 			out_invs.append(str(idcr_reformatted))  # idc rate
+
+			# check if end date is greater than start date; make a log entry if true
+			dates_diff = text_to_date(end_date) - text_to_date(start_date)
+			if dates_diff <= timedelta(days = 0):
+				log_file.write('Inv id=' + str(round(inv[0])) + ' has a strange Start-End date range\n')
 
 			out_inv = '|'.join(out_invs)
 			outfile_invs.write(out_inv + '\n')
@@ -675,8 +793,13 @@ for sub in subs:
 
 					prior_exp += amount
 
+to_print = 'Subs dropped for having empty budgets...'; print(to_print, end='')
+print(padded_text(subs_dropped_for_zero_budgets, len(to_print)))
+to_print = 'Invs dropped for having zero totals...'; print(to_print, end='')
+print(padded_text(invs_dropped_for_zero_amount, len(to_print)))
+
 to_print = 'Output records created - subs:'; print(to_print, end='')
-print(padded_text(len(subs), len(to_print)))
+print(padded_text(output1_count, len(to_print)))
 to_print = 'Output records created - subs_details:'; print(to_print, end='')
 print(padded_text(output2_count, len(to_print)))
 to_print = 'Output records created - invs:'; print(to_print, end='')
